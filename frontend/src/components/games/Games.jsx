d557151c44f40be37ea2b01028e1ff7483403cf5
tareqitos <contact@tareqitos.com>
2025-01-16 12:03:32 +0100

    css changes

--- frontend/src/components/games/Games.jsx
+++ frontend/src/components/games/Games.jsx
@@ -1,30 +1,32 @@
 import { useState, useEffect, use } from 'react'
 import styles from './Games.module.scss'
+import { getGameApi } from '../../utils/fetch-api'
+
 
 function Games() {
     const [gamesList, setGamesList] = useState([])
+    const [gamesReq, setGamesReq] = useState({})
     const [nextPage, setNextPage] = useState('')
     const [isLoading, setIsLoading] = useState(true)
 
-    const API_KEY = 'e80d9fdbcf3947059727685ad06c5f1e'
+
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                let response = await fetch(`https://api.rawg.io/api/games?key=${API_KEY}&dates=2020-01-01,2025-01-01`)
-                const result = await response.json()
-
-                setNextPage(result.next)
-                setGamesList(result.results)
-                setIsLoading(false)
+                const games_result = await getGameApi();
+                setNextPage(games_result.next);
+                setGamesList(games_result.results);
+                setIsLoading(false);
 
 
+
+                console.log(games_result)
             } catch (error) {
-                console.error('Error fetching JSON:', error);
-                setIsLoading(false)
+                console.error("Error fetching JSON:", error);
+                setIsLoading(false);
             }
-        };
-
+        }
         fetchData();
     }, []);
 
@@ -44,13 +46,13 @@ function Games() {
                             </div>
                             <div className={styles['game-infos-container']}>
                                 <div className={styles['game-infos']}>
-                                    <h2>{game.name}</h2>
-                                    {game.genres.map((genre) => (
-                                        <p key={genre.id}>{genre.name}</p>
+                                    <h2 className={styles['game-title']}>{game.name}</h2>
+                                    {game.genres.slice(0, 3).map((genre) => (
+                                        <p className={styles['game-genres']} key={genre.id}>{genre.name}</p>
                                     ))}
                                 </div>
                                 <div className={styles['game-requirements']}>
-
+                                    <p>{game.minimum ? game.minimum['Processor'] : 'No minimum requirements available'}</p>
                                 </div>
                             </div>
                         </div>
