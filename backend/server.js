1efdebf1e898b2ac20ebd110206b0501725bc38e
tareqitos <contact@tareqitos.com>
2025-01-16 12:03:10 +0100

    Add code to fetch from IGDB api

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,47 @@
-const express = require('express');
+const express = require("express");
+const fetch = require("node-fetch");
+require("dotenv").config(); // Load environment variables from .env file
+
 const app = express();
+app.use(express.json()); // Middleware to parse JSON requests
 
-//app.set ('view engine', 'ejs');  --MIGHT need a view engine..will see
+const CLIENT_ID = process.env.IGDB_CLIENT;
+const CLIENT_SECRET = process.env.IGDB_SECRET;
+let token;
+const body = 'fields name,release_dates.*,cover.*,genres.*; where name = "Halo 5: Guardians";';
 
-app.get('/', (req, res) =>{
+async function getToken() {
+  const token_url = `https://id.twitch.tv/oauth2/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials`;
+  const response = await fetch(token_url, { method: "POST" });
+  const result = await response.json();
+  console.log(result.access_token);
+  return result.access_token;
+}
 
-    console.log('Hello Test');
-    res.send('This is a response test');
-    //Some things we are going to need to use below for reference
+async function getData() {
+  const token = await getToken();
+  console.log(token);
+  const url = "https://api.igdb.com/v4/games";
 
-    //res.render('pathToFileYouWantToRenderHere'); example: res.render("index")
-    //res.download('path/toFiletoDownloadHere');
-    //res.status(500);  --Send error status
-    //res.status(500).send('TextString');  -- send status AND custom text string
-    //res.status(500).json({message: "error"}); --send json data and error
-    //res.json({message: "JSON message"})  -- successful json data send
-    
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Client-ID": CLIENT_ID,
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json"
+    },
+    body: body
+  });
+
+  const data = await response.json();
+  console.log(data);
+}
+
+app.get('/games', (req, res) => {
+  const data = getData();
+  res.json(data);
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
-
-app.listen(3000);
-
