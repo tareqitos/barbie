1277889c88f75c87030bcb9546cc40d7c97cca40
iMomobxl <mohamed.alibacha@gmail.com>
2025-01-20 13:40:59 +0100

    add log and error system

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,41 @@
+const path = require("path")
 const express = require("express");
 const fetch = require("node-fetch");
 const cors = require("cors");
+
+const { logger } = require('./middleware/logEvents')
+const errorHandler = require('./middleware/errorHandler')
+
 require("dotenv").config({ path: "../.env" });
-
-const app = express();
-app.use(express.json()); // Middleware to parse JSON requests
-app.use(cors()); // Enable CORS for all routes
-
-console.log("RAWG_API:", process.env.RAWG_API);
-
 const RAWG_API = process.env.RAWG_API;
+const PORT = process.env.PORT || 3000
+const app = express();
+
+app.use(logger)
+
+const whitelist = ['http://localhost', 'http://127.0.0.1']
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by cors'))
+    }
+  },
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions)); // Enable CORS for all routes
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json()); // Middleware to parse JSON requests
+app.use(express.static(path.join(__dirname, '/public')))
+
+//console.log("RAWG_API:", process.env.RAWG_API);
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
 async function getGameData() {
   app.get("/games", async (req, res) => {
@@ -31,6 +57,19 @@ async function getGameData() {
 
 getGameData();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.all('*', (req, res) => {
+  res.status(404)
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (req.accepts('json')) {
+    res.json({ error: "404 Not Found" })
+  } else {
+    res.type('txt').send("404 Not Found")
+  }
+})
+
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
